5a7757976197d5b4097f0e5817398f4f647cae91
Amrit Tiwari <amrit.tiwari@mochilabs.com.au>
2024-02-15 19:14:34 +1100

    Initial Commit

---
+++ Memorizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorizer
+//
+//  Created by Amrit Tiwari on 15/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Amrit Tiwari on 15/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
